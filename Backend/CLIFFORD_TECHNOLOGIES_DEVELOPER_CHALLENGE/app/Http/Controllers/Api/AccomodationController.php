de3b978c6874b1a97a56fab358b4c7ba6748ba26
Trevor-crypto243 <otitrevor450@gmail.com>
2024-01-30 18:50:16 +0300

    Added the api routes

---
+++ Backend/CLIFFORD_TECHNOLOGIES_DEVELOPER_CHALLENGE/app/Http/Controllers/Api/AccomodationController.php
@@ -0,0 +1,9 @@
+<?php
+// AccommodationController.php
+public function index() { ... }
+public function show($id) { ... }
+public function store(Request $request) { ... }
+public function update(Request $request, $id) { ... }
+public function destroy($id) { ... }
+
+// Similar methods in TravelAgentController and ContractController
