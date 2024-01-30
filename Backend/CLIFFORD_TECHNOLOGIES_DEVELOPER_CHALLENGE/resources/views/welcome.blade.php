8d7bec7bfa74c0e67d8cafb952bd1f6b573a4e54
Trevor-crypto243 <otitrevor450@gmail.com>
2024-01-30 18:22:27 +0300

    Laravel and vue js set up

---
+++ Backend/CLIFFORD_TECHNOLOGIES_DEVELOPER_CHALLENGE/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+    <div id="app">
+        <example-component>
+            <h1>Hellooo</h1>
+        </example-component>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
